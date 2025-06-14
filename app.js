40b442cb20621f48afefe2d86134a9aaa2ae3302
dc22v7q521 <trungdc22v7q521@vlvh.ctu.edu.vn>
2025-06-14 10:10:39 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ app.use("/api/contacts", contactsRouter);
 
 // Xử lý route không tồn tại
 app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
+  return next(new ApiError(404, "Resource not found"));
 });
-    
+
 // Xử lý lỗi
 app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
-    
 
 app.get("/", (req, res) => {
-res.json({ message: "Welcome to contact book application." }); });
+  res.json({ message: "Welcome to contact book application." });
+});
 
 module.exports = app;
